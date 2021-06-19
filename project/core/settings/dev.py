f3a6bd9e02ea2d5335bbe17b061534c1a492656a
Brylie Christopher Oxley <brylie@amble.fi>
2021-06-19 11:27:39 +0300

    Initial project scaffold

---
+++ project/core/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-*cy9fa1v3yg$s8y8*qv1icnb)m4nws0!om42$kbg+1i_apq@j7'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
