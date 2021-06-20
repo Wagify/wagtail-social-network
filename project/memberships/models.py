d317513ec1beb3c1c9151a41372538dcac9f3dc7
Brylie Christopher Oxley <brylie@amble.fi>
2021-06-20 20:18:41 +0300

    Initial Membership model

--- project/memberships/models.py
+++ project/memberships/models.py
@@ -1,3 +1,25 @@
+from django.conf import settings
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Membership(models.Model):
+    class MembershipStatusChoices(models.TextChoices):
+        PENDING = "Pending", _("Pending")
+        ACTIVE = "Active", _("Active")
+        LAPSED = "Lapsed", _("Lapsed")
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    chapter = models.ForeignKey(
+        "chapters.Chapter",
+        on_delete=models.CASCADE,
+    )
+    status = models.CharField(
+        max_length=32,
+        choices=MembershipStatusChoices.choices,
+        null=True,
+        blank=True,
+    )
