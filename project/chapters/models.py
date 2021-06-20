bb10d1db8683e2ca8e5978b98a5bf7fea12a67ce
Brylie Christopher Oxley <brylie@amble.fi>
2021-06-20 10:53:21 +0300

    Initial Chapter model

--- project/chapters/models.py
+++ project/chapters/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import RichTextField
+from wagtail.core.models import Page
+
 # Create your models here.
+class Chapter(Page):
+    description = RichTextField()
+
+    content_panels = Page.content_panels + [
+        FieldPanel("description", classname="full"),
+    ]
