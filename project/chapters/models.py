46946caf94c4894f4bdf565ed9fcf070f96b7627
Brylie Christopher Oxley <brylie@amble.fi>
2021-06-20 11:10:43 +0300

    Add Chapter.region field

--- project/chapters/models.py
+++ project/chapters/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.core.fields import RichTextField
@@ -6,8 +7,24 @@ from wagtail.core.models import Page
 
 # Create your models here.
 class Chapter(Page):
+    class RegionChoices(models.TextChoices):
+        AFRICA = "Africa", _("Africa")
+        ANTARCTICA = "Antarctica", _("Antarctica")
+        ASIA = "Asia", _("Asia")
+        AUSTRALIA = "Australia", _("Australia")
+        EUROPE = "Europe", _("Europe")
+        NORTH_AMERICA = "North America", _("North America")
+        SOUTH_AMERICA = "South America", _("South America")
+
     description = RichTextField()
+    region = models.CharField(
+        max_length=32,
+        choices=RegionChoices.choices,
+        null=True,
+        blank=True,
+    )
 
     content_panels = Page.content_panels + [
         FieldPanel("description", classname="full"),
+        FieldPanel("region"),
     ]
