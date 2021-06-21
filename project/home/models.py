c121c59c0f46ba50ba517a1473cdcc55f202258f
Upasana <upasana.dhameliya@gmail.com>
2021-06-21 15:53:44 +0530

    Changed the RichTextField to CharField for the intro
    Made changes in HomePage model for the intro field

--- project/home/models.py
+++ project/home/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
-    intro = RichTextField(blank=True)
+    intro = models.CharField(max_length=250)
+    max_count = 1
 
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname="full"),
