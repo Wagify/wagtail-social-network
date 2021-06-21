631238dc5d5e2f60f2b8671daca4fc1cebe94d74
Brylie Christopher Oxley <brylie@amble.fi>
2021-06-21 12:03:34 +0300

    Add UserChooserViewSet

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.utils.translation import gettext_lazy as _
 
-# Create your views here.
+from generic_chooser.views import ModelChooserViewSet
+
+from .models import User
+
+class UserChooserViewSet(ModelChooserViewSet):
+    icon = "user"
+    model = User
+    page_title =  _("Choose a person")
+    per_page = 10
+    order_by = ("last_name", "first_name")
+    fields = ("first_name", "last_name", "username",)
