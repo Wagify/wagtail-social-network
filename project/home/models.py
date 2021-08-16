dcc6d936c1845247bfe062d1e01ce74b0833407b
Upasana <upasanadtridhyatech@gmail.com>
2021-08-16 17:48:17 +0530

    Added Invite Friends Page in models
    The page was added in the home page models. The page
    would later be used to send out friend invitations
    in order to register with the social media website.
    
    To do the same the Invite Friends page should be
    created from the admin and be made a child of
    the home page for the link to get reflected on the
    home page.

--- project/home/models.py
+++ project/home/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 from wagtail.core.models import Page
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import RichTextField
+
+from django.shortcuts import render
 
 
 class HomePage(Page):
@@ -11,3 +14,43 @@ class HomePage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname="full"),
     ]
+
+    def get_context(self,request):
+        context = super().get_context(request)
+        context['invite_friends'] = self.get_children().type(InviteFriendsPage)
+        print(context)
+        return context
+
+class FriendInvitation(models.Model):
+    email = models.EmailField(max_length=254,blank=True,default=None)
+
+
+class InviteFriendsPage(Page):
+
+    intro = RichTextField(blank=True,null=True,default="")
+    thankyou_page_title = models.CharField(
+        max_length=255, help_text="Title text to use for the 'thank you' page",blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full"),
+        FieldPanel('thankyou_page_title'),
+    ]
+
+    def serve(self, request):
+        from home.forms import FriendInvitationForm
+
+        if request.method == 'POST':
+            form = FriendInvitationForm(request.POST)
+            if form.is_valid():
+                friend_invite = form.save()
+                return render(request, 'home/thankyou.html', {
+                    'page': self,
+                    'flavour': friend_invite,
+                })
+        else:
+            form = FriendInvitationForm()
+
+        return render(request, 'home/invite_friends.html', {
+            'page': self,
+            'form': form,
+        })
