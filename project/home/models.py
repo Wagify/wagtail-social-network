d9596f199df80ecbebc92d092bff798a8b88a687
Johannes Daub <polynoman@live.de>
2021-10-21 21:52:57 +0200

    Add content field to HomePage model #15

--- project/home/models.py
+++ project/home/models.py
@@ -1,19 +1,28 @@
 from django.db import models
 
 from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import StreamField
+from wagtail.core import blocks
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.images.blocks import ImageChooserBlock
 from invites.models import InviteFriendsPage
 
 
 class HomePage(Page):
-    intro = models.CharField(max_length=250,default="Introduction Line")
+    intro = models.CharField(max_length=250, default="Introduction Line")
+    content = StreamField([
+        ('heading', blocks.CharBlock(form_classname="full title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ])
     max_count = 1
 
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname="full"),
+        StreamFieldPanel('content'),
     ]
 
-    def get_context(self,request):
+    def get_context(self, request):
         context = super().get_context(request)
         context['invite_friends_page'] = InviteFriendsPage.objects.get()
         print(context)
