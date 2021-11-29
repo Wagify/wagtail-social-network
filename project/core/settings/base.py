a22d8ffec04837260d296c00c0166d716ad78c2b
Upasana Dhameliya <upasana.dhameliya@gmail.com>
2021-11-29 10:20:06 +0530

    Added locale paths and middleware to settings

--- project/core/settings/base.py
+++ project/core/settings/base.py
@@ -62,6 +62,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -139,6 +140,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = (os.path.join(BASE_DIR, "locale"),)
 
 
 # Static files (CSS, JavaScript, Images)
