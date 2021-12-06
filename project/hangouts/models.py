41153de4f847f97c84b368d300a04ba062801df4
Upasana Dhameliya <upasana.dhameliya@gmail.com>
2021-12-06 12:44:24 +0530

    Added hangouts index page
    Hangouts index page will be used to display all the hangouts
    and will also be used to create new hangouts in order for people
    to collaborate.

--- project/hangouts/models.py
+++ project/hangouts/models.py
@@ -1,11 +1,31 @@
 from django.db import models
 from taggit.managers import TaggableManager
+from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.core.fields import RichTextField
+from wagtail.core.models import Page
 
 
 # Create your models here.
-class Hangout(models.Model):
-    title = models.CharField(max_length=200)
+class Hangout(Page):
     description = RichTextField()
     topics = TaggableManager()
     url = models.URLField(blank=True, null=True)
+
+
+class HangoutsIndexPage(Page):
+    introduction = RichTextField()
+
+    content_panels = Page.content_panels + [
+        FieldPanel("introduction"),
+    ]
+
+    parent_page_types = [
+        "home.HomePage",
+    ]
+
+    max_count = 1
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+        context["hangouts"] = Hangout.objects.all()
+        return context
